0ec1601ad4c5a9a5dc80f394297d4f9873708502
Ismael-Vasco <ismaelvascog@gmail.com>
2026-04-10 17:42:26 -0500

    Deafaul

--- App/obj/Debug/net10.0/App.AssemblyInfo.cs
+++ App/obj/Debug/net10.0/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1c4b891a670583f562ea559861bc983cf74b225")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1449fe3bef3808c327656a58e628ef3a27b5da1")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
